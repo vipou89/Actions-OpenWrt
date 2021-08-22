d9af2fa756ad1b668bcb5783e705e9ba13018fa7
喃南下 <airomeoe@gmail.com>
2021-08-22 21:24:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,11 +11,10 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git OpenAppFilter https://github.com/destan19/OpenAppFilter' feeds.conf.default
 git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
 git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
@@ -31,5 +30,3 @@ git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 # echo "luci-app-openclash" >> .git/info/sparse-checkout
 # git pull origin master
 # git branch --set-upstream-to=origin/master master
-
-sed -i '$a src-git lean https://github.com/coolsnowwolf/lede' feeds.conf.default
