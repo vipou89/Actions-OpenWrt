586adf35db10440cd4cf98e512207e25fec63aff
喃南下 <airomeoe@gmail.com>
2021-08-24 14:57:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,7 +19,7 @@ sed -i '$a src-git OpenAppFilter https://github.com/destan19/OpenAppFilter' feed
 git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
 git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
 git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # mkdir package/luci-app-openclash
